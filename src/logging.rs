284e7478f6784a7a359135ccb9210a3e3a5d7bc9
AkyLich1328 <73415294+AkyLich1328@users.noreply.github.com>
2026-05-14 14:41:01 +0300

    Update logging.rs

--- src/logging.rs
+++ src/logging.rs
@@ -1,9 +1,9 @@
 use std::fmt::Display;
 pub fn log_msg(message: impl Display) {
-    let now = chrono::Local::now();
+    //let now = chrono::Local::now();
     println!(
-        "[{}] Power-Profile - {}",
-        now.format("%Y-%m-%d %H:%M:%S"),
+        "Power-Profile - {}",
+        //now.format("%Y-%m-%d %H:%M:%S"),
         message
     );
 }
