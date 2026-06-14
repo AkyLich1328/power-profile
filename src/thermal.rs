42b3135a9298908d6a6cf0487a978378604b8586
AkyLich1328 <73415294+AkyLich1328@users.noreply.github.com>
2026-06-15 02:13:55 +0300

    Update thermal.rs

--- src/thermal.rs
+++ src/thermal.rs
@@ -1,4 +1,6 @@
-use std::{io, path::PathBuf};
+use std::{fs, io, path::PathBuf};
+
+use crate::logging::log_msg;
 /// Ищет thermal zone с типом x86_pkg_temp
 pub fn find_cpu_temp_path() -> Result<PathBuf, io::Error> {
     for entry in std::fs::read_dir("/sys/class/thermal")? {
@@ -36,4 +38,35 @@ pub async fn get_cpu_temperature(temp_path: &std::path::Path) -> Result<f32, io:
         .map_err(|_| io::Error::other("Ошибка парсинга: cpu temp"))?;
 
     Ok(temp / 1000.0)
+}
+
+pub fn set_intel_turbo_bost(disable: bool) -> Result<(), std::io::Error> {
+    let path = "/sys/devices/system/cpu/intel_pstate/no_turbo";
+    let value: u8 = fs::read_to_string(path)
+        .expect("Не удалось прочитать файл no_turbo")
+        .trim()
+        .parse()
+        .expect("В файле должно быть число");
+
+    match value {
+        1 => {
+            if !disable {
+                match fs::write(path, "0") {
+                    Ok(()) => log_msg("Успешно включаем TurboBoost"),
+                    Err(e) => println!("Ошибка TurboBoost: {}", e),
+                }
+            }
+        }
+        0 => {
+            if disable {
+                match fs::write(path, "1") {
+                    Ok(()) => log_msg("Успешно отключаем TurboBoost"),
+                    Err(e) => println!("Ошибка TurboBoost: {} ", e),
+                }
+            }
+        }
+        _ => {}
+    }
+
+    Ok(())
 }
